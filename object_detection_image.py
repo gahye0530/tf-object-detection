8520fdb16cfff71e51fcba6150dce0cdc03f7b7d
gahye0530 <40849083+gahye0530@users.noreply.github.com>
2021-12-29 17:28:12 +0900

    12280528

--- object_detection_image.py
+++ object_detection_image.py
@@ -8,14 +8,33 @@ from PIL import Image
 import cv2
 from object_detection.utils import ops as utils_ops
 from object_detection.utils import label_map_util
-from object_detection.utils import visualization_utils as vis_util
-
-from plot_object_detection_saved_model import IMAGE_PATHS
+from object_detection.utils import visualization_utils as viz_utils
+from plot_object_detection_saved_model import IMAGE_PATHS, download_model
 
 # 경로세팅
 PATH_TO_LABELS = 'C:\\Users\\5-18\\Documents\\TensorFlow\\models-master\\research\\object_detection\\data\\mscoco_label_map.pbtxt'
-CATEGORY_INDEX = label_map_util.create_categories_from_labelmap(PATH_TO_LABELS)
+category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS)
+
+# 모델 로드하는 함수.
+
+# https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/tf2_detection_zoo.md
+# 위의 사이트에서 모델을 가져올수있다.
+
+# /20200711/efficientdet_d0_coco17_tpu-32.tar.gz
+
+# Download and extract model
+def download_model(model_name, model_date):
+    base_url = 'http://download.tensorflow.org/models/object_detection/tf2/'
+    model_file = model_name + '.tar.gz'
+    model_dir = tf.keras.utils.get_file(fname=model_name,
+                                        origin=base_url + model_date + '/' + model_file,
+                                        untar=True)
+    return str(model_dir)
+
+MODEL_DATE = '20200711'
+MODEL_NAME = 'centernet_hg104_1024x1024_coco17_tpu-32'
 PATH_TO_MODEL_DIR = download_model(MODEL_NAME, MODEL_DATE)
+
 
 def load_model(model_dir) :
     model_full_dir = model_dir + "/saved_model"  
@@ -30,7 +49,63 @@ print(detection_model.signatures['serving_default'].output_dtypes)
 print(detection_model.signatures['serving_default'].output_shapes)
 
 # 우리가 가지고 있는 이미지 경로에서 이미지를 가져오는 코드
-PATH_TO_IMAGE_DIR = pathlib.Path('data')
+PATH_TO_IMAGE_DIR = pathlib.Path('data\\images')
 IMAGE_PATHS = list(PATH_TO_IMAGE_DIR.glob('*.jpg'))
 
+def load_image_into_numpy_array(path):
+    # return np.array(Image.open(path))
+    print(str(path))
+    return cv2.imread(str(path))
+
+
 print(IMAGE_PATHS)
+
+for image_path in IMAGE_PATHS:
+
+    print('Running inference for {}... '.format(image_path), end='')
+
+    image_np = load_image_into_numpy_array(image_path)
+
+    # Things to try:
+    # Flip horizontally
+    # image_np = np.fliplr(image_np).copy()
+
+    # Convert image to grayscale
+    # image_np = np.tile(
+    #     np.mean(image_np, 2, keepdims=True), (1, 1, 3)).astype(np.uint8)
+
+    # The input needs to be a tensor, convert it using `tf.convert_to_tensor`.
+    input_tensor = tf.convert_to_tensor(image_np)
+    # The model expects a batch of images, so add an axis with `tf.newaxis`.
+    input_tensor = input_tensor[tf.newaxis, ...]
+
+    # input_tensor = np.expand_dims(image_np, 0)
+    detections = detection_model(input_tensor)
+
+    # All outputs are batches tensors.
+    # Convert to numpy arrays, and take index [0] to remove the batch dimension.
+    # We're only interested in the first num_detections.
+    num_detections = int(detections.pop('num_detections'))
+    detections = {key: value[0, :num_detections].numpy()
+                   for key, value in detections.items()}
+    detections['num_detections'] = num_detections
+
+    # detection_classes should be ints.
+    detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
+
+    image_np_with_detections = image_np.copy()
+
+    viz_utils.visualize_boxes_and_labels_on_image_array(
+          image_np_with_detections,
+          detections['detection_boxes'],
+          detections['detection_classes'],
+          detections['detection_scores'],
+          category_index,
+          use_normalized_coordinates=True,
+          max_boxes_to_draw=200,
+          min_score_thresh=.30,
+          agnostic_mode=False)
+
+    cv2.imshow(str(image_path), image_np_with_detections)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
